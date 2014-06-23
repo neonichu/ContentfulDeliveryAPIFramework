4ae36b80931f70491e7692acdcf297da2cd741f8
Boris Bügling <boris@icculus.org>
2014-06-23 14:08:18 +0200

    Initial commit

---
+++ ContentfulDeliveryAPI/ContentfulDeliveryAPI.h
@@ -0,0 +1,30 @@
+//
+//  ContentfulDeliveryAPI.h
+//  ContentfulDeliveryAPI
+//
+//  Created by Boris Bügling on 23/06/14.
+//  Copyright (c) 2014 Boris Bügling. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ContentfulDeliveryAPI.
+FOUNDATION_EXPORT double ContentfulDeliveryAPIVersionNumber;
+
+//! Project version string for ContentfulDeliveryAPI.
+FOUNDATION_EXPORT const unsigned char ContentfulDeliveryAPIVersionString[];
+
+#import <ContentfulDeliveryAPI/CDAArray.h>
+#import <ContentfulDeliveryAPI/CDAAsset.h>
+#import <ContentfulDeliveryAPI/CDAClient.h>
+#import <ContentfulDeliveryAPI/CDAConfiguration.h>
+#import <ContentfulDeliveryAPI/CDAContentType.h>
+#import <ContentfulDeliveryAPI/CDAEntry.h>
+#import <ContentfulDeliveryAPI/CDAField.h>
+#import <ContentfulDeliveryAPI/CDAPersistenceManager.h>
+#import <ContentfulDeliveryAPI/CDARequest.h>
+#import <ContentfulDeliveryAPI/CDAResponse.h>
+#import <ContentfulDeliveryAPI/CDASpace.h>
+#import <ContentfulDeliveryAPI/CDASyncedSpace.h>
+
+extern NSString* const CDAErrorDomain;
---
+++ Podfile
@@ -0,0 +1,19 @@
+platform :ios, '7.0'
+platform :osx, '10.9'
+
+target "ContentfulDeliveryAPI" do
+
+pod 'AFNetworking'
+pod 'HRCoder'
+pod 'ISO8601DateFormatter'
+
+end
+
+target "ContentfulDeliveryAPI-Mac" do
+
+pod 'AFNetworking'
+pod 'HRCoder'
+pod 'ISO8601DateFormatter'
+
+end
+
